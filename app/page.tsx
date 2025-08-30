4ac41246df3070c79c1a7f5e978272fb91969752
Andreas Fagerberg <andreas_fagerberg@outlook.com>
2025-08-30 14:20:29 +0200

    feat: Add form for input, implement react useState
    - Add form for users to enter name and email
    - useState implemented to help handle tracking input

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,41 @@
-import Link from "next/link";
+'use client'
+import React, { useState } from "react";
+export default async function Home() {
+  const [newName, setNewName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
 
-export default function Home() {
+  const checkIn = () => {
+    const name = newName.trim();
+    const email = newEmail.trim();
+    // if (name && email) {
+    //   fetch(
+    //     "", // Add api url here later.
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify({
+    //         name,
+    //         email,
+    //       }),
+    //       headers: {
+    //         "Content-type": "application/json; charset=UTF-8",
+    //       },
+    //     }
+    //   );
+    // }
+    if (name && email)
+    {
+      alert("success!")
+    }
+  };
   return (
     <div>
-      Hello World. <Link href="/about">About</Link>
+      <form action={checkIn}>
+        <input type="text" name="name" value={newName} onChange={e => setNewName(e.target.value)}
+        placeholder="Enter name here..." />
+        <input type="text" name="email" value={newEmail} onChange={e => setNewEmail(e.target.value)}
+        placeholder="Enter email here..." />
+        <button type="submit">Check in</button>
+      </form>
     </div>
   );
 }

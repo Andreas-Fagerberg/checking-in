3160ec985473481a976ecc57311e7f5243516942
Andreas Fagerberg <andreas_fagerberg@outlook.com>
2025-09-05 17:49:23 +0200

    Feat: Add Fetch API for POST action
    - Implement fetch api and state management for
    handling data
    - Added elements to display errors to user.

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,49 @@
-'use client'
+"use client";
+import './page.css';
+
 import React, { useState } from "react";
-export default async function Home() {
+export default function Home() {
   const [newName, setNewName] = useState("");
   const [newEmail, setNewEmail] = useState("");
+  const [error, setError] = useState("");
 
-  const checkIn = () => {
+  const checkIn = async () => {
     const name = newName.trim();
     const email = newEmail.trim();
-    // if (name && email) {
-    //   fetch(
-    //     "", // Add api url here later.
-    //     {
-    //       method: "POST",
-    //       body: JSON.stringify({
-    //         name,
-    //         email,
-    //       }),
-    //       headers: {
-    //         "Content-type": "application/json; charset=UTF-8",
-    //       },
-    //     }
-    //   );
-    // }
-    if (name && email)
-    {
-      alert("success!")
+    if (name && email) {
+      const response = await fetch("https://checking-in-funcs-dncrc3c4a4bhe9fm.swedencentral-01.azurewebsites.net/api/CreateUser?", {
+        method: "POST",
+        body: JSON.stringify({
+          Name: name,
+          Email: email,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+
+      if (!response.ok)
+      {
+        const data = await response.json();
+        setError(data.error);
+      }
+      
+      
     }
   };
   return (
-    <div>
-      <form action={checkIn}>
-        <input type="text" name="name" value={newName} onChange={e => setNewName(e.target.value)}
-        placeholder="Enter name here..." />
-        <input type="text" name="email" value={newEmail} onChange={e => setNewEmail(e.target.value)}
-        placeholder="Enter email here..." />
-        <button type="submit">Check in</button>
-      </form>
+    <div className="checkin-main-container">
+      <div className="checkin-form-container">
+        <form className="checkin-form"action={checkIn}>
+          <input type="text" name="name" value={newName} onChange={(e) => setNewName(e.target.value)} placeholder="Enter name here..." />
+          <input type="text" name="email" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} placeholder="Enter email here..." />
+          <button type="submit">Check in</button>
+        </form>
+        <p className="checkin-error-text">
+        {error}
+      </p>
+      </div>
+      
     </div>
   );
 }

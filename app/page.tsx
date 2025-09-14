1e48e2d818818ecc0fb18e02f76f9430078f65b0
Andreas Fagerberg <andreas_fagerberg@outlook.com>
2025-09-14 21:40:17 +0200

    feat: Implement user input functionality and error handling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,59 @@
 "use client";
-import './page.css';
+import "./page.css";
+import React, { useEffect, useState } from "react";
 
-import React, { useState } from "react";
 export default function Home() {
   const [newName, setNewName] = useState("");
   const [newEmail, setNewEmail] = useState("");
-  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("");
 
+  useEffect(() => {
+    setNewName("");
+    setNewEmail("");
+  }, []);
   const checkIn = async () => {
     const name = newName.trim();
     const email = newEmail.trim();
-    if (name && email) {
-      const response = await fetch("https://checking-in-funcs-dncrc3c4a4bhe9fm.swedencentral-01.azurewebsites.net/api/CreateUser?", {
-        method: "POST",
-        body: JSON.stringify({
-          Name: name,
-          Email: email,
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      });
+    if (!name || !email) {
+      setMessage("Name and email is required");
+      setMessageType("checkin-error-message");
+      return;
+    }
 
-      if (!response.ok)
-      {
-        const data = await response.json();
-        setError(data.error);
-      }
-      
-      
+    const response = await fetch("https://checking-in-funcs-dncrc3c4a4bhe9fm.swedencentral-01.azurewebsites.net/api/CreateUser?", {
+      method: "POST",
+      body: JSON.stringify({
+        Name: name,
+        Email: email,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+
+    if (response.ok) {
+      setMessage("User succesfully checked in");
+      setMessageType("checkin-success-message");
+      setNewEmail("");
+      setNewName("");
+    } else {
+      const data = await response.json();
+      setMessage(data.error);
+      setMessageType("checkin-error-message");
     }
   };
+
   return (
     <div className="checkin-main-container">
       <div className="checkin-form-container">
-        <form className="checkin-form"action={checkIn}>
+        <form className="checkin-form" action={checkIn}>
           <input type="text" name="name" value={newName} onChange={(e) => setNewName(e.target.value)} placeholder="Enter name here..." />
-          <input type="text" name="email" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} placeholder="Enter email here..." />
+          <input type="email" name="email" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} placeholder="Enter email here..." />
           <button type="submit">Check in</button>
+          <p className={messageType}>{message}</p>
         </form>
-        <p className="checkin-error-text">
-        {error}
-      </p>
       </div>
-      
     </div>
   );
 }

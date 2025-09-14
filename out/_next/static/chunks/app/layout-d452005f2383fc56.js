83b5217d4eb8396ed0389a06cf3f976d892be4ed
Andreas Fagerberg <andreas_fagerberg@outlook.com>
2025-09-14 21:36:19 +0200

    Deploy

---
+++ out/_next/static/chunks/app/layout-d452005f2383fc56.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2010:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,6611,23)),Promise.resolve().then(l.t.bind(l,5003,23))},5003:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_277b82"}},6611:()=>{}},e=>{e.O(0,[748,211,441,255,358],()=>e(e.s=2010)),_N_E=e.O()}]);
